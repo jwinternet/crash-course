9a5e7217d07616582e3bb47fcc9acbf194fd0d93
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 16:34:26 -0500

    Updated the person.py file.

--- Chapter 8/person.py
+++ Chapter 8/person.py
@@ -1,7 +1,11 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None):
 	person = {"first": first_name, "last": last_name}
+	if age:
+		person["age"] = age
 	return person
 
 
-musician = build_person("jimi", "hendrix")
+musician = build_person("jimi", "hendrix", age=27)
 print(musician)
+deadbeat = build_person("ronald", "mcdonald")
+print(deadbeat)

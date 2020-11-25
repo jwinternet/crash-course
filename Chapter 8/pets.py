dd9047c53d17383bbaec7c67b7de5a9d058847f7
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 15:15:44 -0500

    Updated details to pets.py file.

--- Chapter 8/pets.py
+++ Chapter 8/pets.py
@@ -1,6 +1,7 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(animal_type, pet_name="jarvis"):
 	print(f"\nI have a {animal_type}.")
 	print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 
-describe_pet("pig", "Abner")
+describe_pet(pet_name="abner", animal_type="pig")
+describe_pet(animal_type="rat")

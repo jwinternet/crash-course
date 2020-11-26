9725a5cd632d6c6e17f55ed21379ed340a4cb073
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 22:27:26 -0500

    I transformed my old normal boring car into a hot sexy electric car, just don't ask how!

--- Chapter 9/car.py
+++ Chapter 9/car.py
@@ -22,14 +22,46 @@ class Car:
 		self.odometer_reading += miles
 
 
+class ElectricCar(Car):
+	def __init__(self, make, model, year):
+		super().__init__(make, model, year)
+		self.battery = Battery()
+		self.battery_size = 75
+
+	def describe_battery(self):
+		print(f"This car has a {self.battery_size}-kWh battery.")
+
+
+class Battery:
+	def __init__(self, battery_size=75):
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		print(f"This car has a {self.battery_size}-kWh battery.")
+
+	def get_range(self):
+		if self.battery_size == 75:
+			range = 260
+		elif self.battery_size == 100:
+			range = 315
+		print(f"This car can go about {range} miles on a full charge.")
+
+
 my_new_car = Car("Toyota", "Camry", 2012)
 print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(12210)
 my_new_car.read_odometer()
 
-my_used_car = Car('subaru', 'outback', 2015)
+my_used_car = Car("subaru", "outback", 2015)
 print(my_used_car.get_descriptive_name())
 my_used_car.update_odometer(21600)
 my_used_car.read_odometer()
 my_used_car.increment_odometer(7156)
 my_used_car.read_odometer()
+
+my_tesla = ElectricCar("tesla", "model s", 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()

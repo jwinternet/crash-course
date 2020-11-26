395969bc4eb279b68d6cad3761bdf73f037d39e9
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 21:50:50 -0500

    Further expanded the functionality for the car.py file.

--- Chapter 9/car.py
+++ Chapter 9/car.py
@@ -6,17 +6,30 @@ class Car:
 		self.odometer_reading = 0
 
 	def get_descriptive_name(self):
-		long_name = f"{self.make} {self.model} {self.year}"
+		long_name = f"\n{self.make} {self.model} {self.year}"
 		return long_name.title()
 
 	def read_odometer(self):
 		print(f"This car has {self.odometer_reading} miles on it.")
 
 	def update_odometer(self, mileage):
-		self.odometer_reading = mileage
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("\nYou can't roll back an odometer!")
+
+	def increment_odometer(self, miles):
+		self.odometer_reading += miles
 
 
 my_new_car = Car("Toyota", "Camry", 2012)
 print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(102123)
+my_new_car.update_odometer(12210)
 my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(21600)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(7156)
+my_used_car.read_odometer()

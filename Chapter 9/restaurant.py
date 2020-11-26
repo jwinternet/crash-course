6d01edd856eb20d101d06f8eaa58d23489f7b218
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 22:08:10 -0500

    Updated the restaurant.py file.

--- Chapter 9/restaurant.py
+++ Chapter 9/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant:
 	def __init__(self, restaurant_name, cuisine_type):
 		self.restaurant_name = restaurant_name
 		self.cuisine_type = cuisine_type
+		self.number_served = 19108141
 
 	def describe_restaurant(self):
 		print(
@@ -12,7 +13,17 @@ class Restaurant:
 	def open_restaurant(self):
 		print(f"{self.restaurant_name} is currently open for business.")
 
+	def served(self):
+		print(f"The restaurant has served {self.number_served} people.")
+
+	def increment_served(self, more_served):
+		self.number_served += more_served
+
 
 restaurant = Restaurant("Wendy's", "burgers")
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+restaurant.served()
+restaurant.increment_served(56465)
+restaurant.served()

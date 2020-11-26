f2d5f3feb1c542944f7d6c17222f52345d7fa9cd
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 23:39:24 -0500

    Updated the user_profile.py class, doesn't seem to want to work at present (Chapter 9, Exercise 9-8).

--- Chapter 9/user_profile.py
+++ Chapter 9/user_profile.py
@@ -21,20 +21,25 @@ class UserProfile:
 		)
 
 
+class Privileges:
+	def __init__(self):
+		self.privileges = ["can add post", "can delete post", "can ban user"]
+
+	def show_privileges(self):
+		print(f"\nThe user has the following privileges:")
+		for p in self.privileges:
+			print(" - " + p)
+
+
 class Admin(UserProfile):
 	def __init__(
 			self, first_name, last_name, join_year, country, number_of_posts):
 		super().__init__(
 			first_name, last_name, join_year, country, number_of_posts)
-		self.privileges = ["can add post", "can delete post", "can ban user"]
-
-	def show_privileges(self):
-		print(f"\nThe Admin has the following privileges:")
-		for p in self.privileges:
-			print(" - " + p)
+		self.privileges = Privileges
 
 
 new_user = Admin("Jeff", "Skilling", 2001, "USA", 2184)
 new_user.describe_user()
 new_user.greet_user()
-new_user.show_privileges()
+new_user.privileges.show_privileges()

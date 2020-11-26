7b1383a42660901b1d9f344329b8d5f92e2f748e
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-25 21:43:47 -0500

    Updated and expanded functionality for the car.py file.

--- Chapter 9/car.py
+++ Chapter 9/car.py
@@ -3,11 +3,20 @@ class Car:
 		self.make = make
 		self.model = model
 		self.year = year
+		self.odometer_reading = 0
 
 	def get_descriptive_name(self):
 		long_name = f"{self.make} {self.model} {self.year}"
 		return long_name.title()
 
+	def read_odometer(self):
+		print(f"This car has {self.odometer_reading} miles on it.")
+
+	def update_odometer(self, mileage):
+		self.odometer_reading = mileage
+
 
 my_new_car = Car("Toyota", "Camry", 2012)
 print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(102123)
+my_new_car.read_odometer()

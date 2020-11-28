9b7c0a5d8ce524e5ec97c243deccf6dd58a11e33
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-11-27 21:42:33 -0500

    Added more features.

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -20,14 +20,21 @@ class AlienInvasion:
 	def run_game(self):
 		while True:
 			self._check_events()
-			self.screen.fill(self.settings.bg_color)
-			self.ship.blitme()
-			pygame.display.flip()
+			self._update_screen()
 
 	def _check_events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					self.ship.rect.x += 1
+
+
+	def _update_screen(self):
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+		pygame.display.flip()
 
 
 if __name__ == '__main__':
